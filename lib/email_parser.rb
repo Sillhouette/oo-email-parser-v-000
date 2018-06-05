49fc09914517345457d91cbea335e1ae873d87b0
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 16:16:47 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailParser
     seperated_emails = @emails.split(/[ ,]/)
     puts seperated_emails
     unique_emails = []
-    unique_emails.map {|email| seperated_emails.include?(email)}
+    seperated_emails.map {|email| unique_emails.include?(email)}
     #seperated_emails.map {|email| unique_emails.include?(email)}
     #unique_emails
   end
